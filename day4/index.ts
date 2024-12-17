2ddd5f8629f7c042c3b435fc7a10efc77e59b53e
Philipp Renner <phillianus@gmail.com>
2024-12-17 14:04:26 +0100

    feat(day4): Part2 - X-MAS Search

--- day4/index.ts
+++ day4/index.ts
@@ -53,9 +53,31 @@ function part1(data: string[]): number {
 	}
 }
 
-//function part2(data: string): number {}
+function part2(data: string[]): number {
+	let count = 0;
+
+	for (let i = 1; i < data.length - 1; i++) {
+		for (let j = 1; j < data[i].length - 1; j++) {
+			if (data[i][j] !== "A") {
+				continue;
+			}
+
+			if (
+				((data[i - 1][j - 1] === "M" && data[i + 1][j + 1] === "S") ||
+					(data[i - 1][j - 1] === "S" &&
+						data[i + 1][j + 1] === "M")) &&
+				((data[i - 1][j + 1] === "M" && data[i + 1][j - 1] === "S") ||
+					(data[i - 1][j + 1] === "S" && data[i + 1][j - 1] === "M"))
+			) {
+				count++;
+			}
+		}
+	}
+
+	return count;
+}
 
 // read data from part1.txt
 const data = readFileSync("part1.txt", "utf8").split("\n");
-console.log(part1(data));
-//console.log(part2(data));
+//console.log(part1(data));
+console.log(part2(data));
